1c240cabba3d84039aaa01c750466e3c774f0845
chagasma <mauro.junior@sou.inteli.edu.br>
2025-01-12 15:44:35 -0300

    :ambulance: fix: terraform outputs.tf

--- src/terraform/modules/dynamodb/outputs.tf
+++ src/terraform/modules/dynamodb/outputs.tf
@@ -1,3 +1,11 @@
 output "table_name" {
   value = aws_dynamodb_table.this.name
 }
+
+output "table_arn" {
+  value = aws_dynamodb_table.this.arn
+}
+
+output "table_id" {
+  value = aws_dynamodb_table.this.id
+}
--- src/terraform/modules/lambda/outputs.tf
+++ src/terraform/modules/lambda/outputs.tf
@@ -1,7 +1,11 @@
-output "lambda_arn" {
-  value = aws_lambda_function.this.arn
+output "function_name" {
+  value = aws_lambda_function.this.function_name
 }
 
-output "lambda_invoke_arn" {
+output "invoke_arn" {
   value = aws_lambda_function.this.invoke_arn
+}
+
+output "function_arn" {
+  value = aws_lambda_function.this.arn
 }
